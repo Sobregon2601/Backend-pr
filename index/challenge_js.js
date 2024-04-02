0bd226a98dbabcdfe3e5c1b99bbd3cdfd963f01e
Mathias Obregon <obregonmathias@hotmail.com>
2024-04-02 14:26:24 -0300

    challenge

---
+++ index/challenge_js.js
@@ -0,0 +1,29 @@
+class ProductManager {
+  products;
+  constructor() {
+    this.title = title; //nombre dell producto
+    this.description = description; // descripcion del producto
+    this.price = price; //precio
+    this.thumbnail = thumbnail; //ruta de imagen
+    this.code = code; //codigo identificador
+    this.stock = stock; //numero de piezas disponibles
+    this.products = `${this.title} ${this.description} ${this.price} ${this.thumbnail} ${this.code} ${this.stock}`;
+  }
+
+  addProduct() {
+    product1 = new ProductManager(
+      "tornillo",
+      "tornillo con rosca",
+      20,
+      (URL = "assets"),
+      1,
+      20
+    );
+  }
+}
+
+getProducts = () => {
+  return this.products;
+};
+
+let producto1 = console.log(product1.getProducts());
