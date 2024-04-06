fabee86d074fea84f317a268d4ac9ec25fe807eb
Mathias Obregon <obregonmathias@hotmail.com>
2024-04-06 13:51:39 -0300

    challenge

---
+++ index/js_challenge.js
@@ -0,0 +1,39 @@
+console.log("que sera");
+
+class ProductManager {
+  constructor() {
+    this.ptoducts = [];
+  }
+  addProduct() {
+    product1 = new ProductManager(
+      "tornillo",
+      "tornillo con rosca",
+      20,
+      (URL = "assets"),
+      1,
+      20
+    );
+    product1 = new ProductManager(
+      "tornillo",
+      "tornillo con rosca",
+      20,
+      (URL = "assets"),
+      1,
+      20
+    );
+    product1 = new ProductManager(
+      "tornillo",
+      "tornillo con rosca",
+      20,
+      (URL = "assets"),
+      1,
+      20
+    );
+  }
+}
+
+getProducts = () => {
+  return this.products;
+};
+
+console.log(product1.getProducts());
---
+++ index/js_pruebas.js
@@ -0,0 +1,93 @@
+/*class ProductManager {
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title; //nombre dell producto
+    this.description = description; // descripcion del producto
+    this.price = price; //precio
+    this.thumbnail = thumbnail; //ruta de imagen
+    this.code = code; //codigo identificador
+    this.stock = stock; //numero de piezas disponibles
+    ///this.products = {this.title} $ {this.description} ${this.price} ${this.thumbnail} ${this.code} ${this.stock};
+  }
+
+
+
+
+      this.title = title; //nombre dell producto
+    this.description = description; // descripcion del producto
+    this.price = price; //precio
+    this.thumbnail = thumbnail; //ruta de imagen
+    this.code = code; //codigo identificador
+    this.stock = stock; //numero de piezas disponibles
+    this.products = `${this.title} ${this.description} ${this.price} ${this.thumbnail} ${this.code} ${this.stock}`;
+  }
+
+
+
+  addProduct() {
+    const product1 = new ProductManager(
+      "tornillo",
+      "tornillo con rosca",
+      20,
+      (URL = "assets"),
+      1,
+      20
+    );
+    const product2 = new ProductManager(
+      "tornillo",
+      "tornillo con rosca",
+      20,
+      (URL = "assets"),
+      1,
+      20
+    );
+
+    getProducts = () => {
+      console.log(product1.addProduct);
+    };
+
+    getProducts();
+  }
+}
+
+/*getProducts = () => {
+  console.log(product1);
+};
+
+getProducts();*/
+class ProdcutManager {
+  constructor() {
+    this.products = [];
+    this.id = 1;
+  }
+
+  addProduct() {
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbnail ||
+      !product.code ||
+      !product.stock
+    ) {
+    }
+
+    getProducts();
+    {
+      return this.products;
+    }
+
+    getProductById(id);
+    {
+    }
+  }
+}
+
+const pm = new ProdcutManager();
+
+pm.addProduct("Producto test 1", "descripcion 1", 11, "img1.jpg", "1111", 1);
+pm.addProduct("Producto test 2", "descripcion 2", 22, "img1.jpg", "2222", 2);
+pm.addProduct("Producto test 3", "descripcion 3", 33, "img1.jpg", "3333", 2);
+
+console.log(pm.getProducts());
+
+console.log(pm.getProductById(1));
