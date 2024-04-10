3edd039e406981dc20ac1df6f4a0566ad845871d
Mathias Obregon <obregonmathias@hotmail.com>
2024-04-10 12:44:44 -0300

    challenge

--- index/js_challenge.js
+++ index/js_challenge.js
@@ -1,33 +1,56 @@
 class ProductManager {
   constructor() {
     this.products = [];
-    this.id = ProductManager.generarId();
+    this.Id = 1;
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
-    this.products.push(title, description, price, thumbnail, code, stock);
-  }
-
-  static generarId() {
-    if (!ProductManager.contador) {
-      ProductManager.contador = 1;
-
-      return ProductManager.contador++;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.log("Todos los campos son obligatorios");
+      return;
     }
+
+    if (this.products.some((products) => products.code === code)) {
+      console.log(`El codigo ${code} de producto ya existe.`);
+      return;
+    }
+
+    const product = {
+      id: this.Id++,
+      title: title,
+      description: description,
+      price: price,
+      thumbnail: thumbnail,
+      code: code,
+      stock: stock,
+    };
+
+    this.products.push(product);
+    console.log("Producto agregado OK.");
   }
 
   getProducts() {
-    return this.products;
+    console.log(this.products);
+  }
+
+  getProductByid(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (product) {
+      console.log(`Producto encontrado : ${product.title}`);
+      console.log(product);
+    } else {
+      console.log("Producto no encontrado");
+    }
   }
 }
 
-const pm = new ProductManager("ProductManager 1");
-pm.addProduct("Producto test 1", "descripcion 1", 11, "assets", 1, 11);
+pm = new ProductManager();
 
-const pm2 = new ProductManager();
-pm2.addProduct("Producto test 2", "descripcion 2", 22, "assets2", 2, 22);
+pm.getProducts();
 
-console.log(pm.getProducts());
-console.log(pm2.getProducts());
+pm.addProduct("Producto", "descripcion", 200, "images.jpg", "COO1", 25);
+pm.addProduct("Producto2", "descripcion2", 300, "images.jpg", "COO1", 25);
+pm.addProduct("Producto3", "descripcion3", 400, "images.jpg", "COO2", 35);
 
-console.log(pm);
+pm.getProductByid(1);
+pm.getProductByid(4);
